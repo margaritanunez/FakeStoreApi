85c1a7556b3912fdce1432168a8a329e064ec4c5
Margaret <maenunezt@gmail.com>
2023-08-30 18:08:40 -0400

    se crea la data class Products, en donde se modelan los datos de la API escritos en JSON a variables en Kotlin.

---
+++ app/src/main/java/com/example/fakestoreapi/data/remote/Products.kt
@@ -0,0 +1,33 @@
+package com.example.fakestoreapi.data.remote
+
+data class Products(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    val image: String,
+    val rating: Rating
+)
+
+data class Rating(
+    val rate: Double,
+    val count: Int
+)
+
+/*
+ {
+        "id": 1,
+        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        "price": 109.95,
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "rating": {
+            "rate": 3.9,
+            "count": 120
+        } rating es un objeto con dos valores(un map). se crea una clase para guardar estis valores
+        la variable rating de la diez  es de tipo Rating.
+        se crea una data class en donde se almacenan las variables rate y count
+    },
+ */
