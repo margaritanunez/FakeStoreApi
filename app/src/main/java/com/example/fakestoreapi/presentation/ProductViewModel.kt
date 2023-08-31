66ca816894e9656cd7bef16386609df0901d155d
Margaret <maenunezt@gmail.com>
2023-08-31 01:24:42 -0400

    creación de viewModel

---
+++ app/src/main/java/com/example/fakestoreapi/presentation/ProductViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.fakestoreapi.presentation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fakestoreapi.data.Repository
+import com.example.fakestoreapi.data.local.ProductDataBase
+import com.example.fakestoreapi.data.remote.ProductRetrofitClient
+import kotlinx.coroutines.launch
+
+class ProductViewModel(application: Application) : AndroidViewModel(application) {
+
+    private lateinit var repository: Repository
+
+    init {
+        val productApi = ProductRetrofitClient.getRetrofitProduct()
+        val productDataBase = ProductDataBase.getDatabase(application).getProductDao()
+        repository = Repository(productApi, productDataBase)
+    }
+
+    fun getAllProductos()= viewModelScope.launch { repository.ChargeAllProducts() }
+}
