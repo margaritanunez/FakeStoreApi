074d478aec530af753891acd8a7df29b38765a2c
Margaret <maenunezt@gmail.com>
2023-08-31 01:02:58 -0400

    creación de la clase Repository

---
+++ app/src/main/java/com/example/fakestoreapi/data/Repository.kt
@@ -0,0 +1,43 @@
+package com.example.fakestoreapi.data
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.example.fakestoreapi.data.local.ProductDao
+import com.example.fakestoreapi.data.local.ProductEntity
+import com.example.fakestoreapi.data.remote.Product
+import com.example.fakestoreapi.data.remote.ProductApi
+
+class Repository(private val productApi: ProductApi, private val productDao: ProductDao)  {
+
+
+    fun getProductEntity(): LiveData<List<ProductEntity>> = productDao.getAllProducts()
+
+    suspend fun ChargeAllProducts() {
+        try {
+            val response = productApi.getDataProduct()
+            if (response.isSuccessful) {
+                val resp = response.body()
+                resp?.let {
+                    val productosEntity = it.map { it.transformToEntity() }
+
+                }
+            }else {
+                Log.e("repository", response.errorBody().toString())
+            }
+        } catch (exception: Exception) {
+            Log.e("catch", "")
+
+        }
+    }
+    private fun Product.transformToEntity(): ProductEntity = ProductEntity(
+        this.id,
+        this.title,
+        this.price,
+        this.description,
+        this.category,
+        this.image,
+        this.rating.rate,
+        this.rating.count
+    )
+}
+
